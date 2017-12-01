72d0aba5a6759fa7775b316723176b68a3c7c5b7
Itay <itay647@gmail.com>
2017-12-01 22:10:23 +0200

    Added a very important comment

--- src/test/MainProgram.java
+++ src/test/MainProgram.java
@@ -5,6 +5,7 @@ public class MainProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		// I am a comment
 		System.out.println("Hello World!!!!");
 	}
 

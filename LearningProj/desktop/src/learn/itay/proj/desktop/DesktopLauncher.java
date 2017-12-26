3a2b87ee38394cf10c0d7650a549b58624e15dcd
Itay <itay647@gmail.com>
2017-12-26 08:15:39 +0200

    Added a new project for learning libgdx & tiled * Main Source: https://www.youtube.com/playlist?list=PLafgA4ncMb23wk1xh0AsbJfGa7h_SEgN_

---
+++ LearningProj/desktop/src/learn/itay/proj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package learn.itay.proj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import learn.itay.proj.LearningProjGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LearningProjGame(), config);
+	}
+}

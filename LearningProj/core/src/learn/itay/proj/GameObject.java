647f1bdb8959982b88d7bd142baefbbe80d0b607
Itay <itay647@gmail.com>
2017-12-27 17:11:53 +0200

    Added GameInput class

--- LearningProj/core/src/learn/itay/proj/GameObject.java
+++ LearningProj/core/src/learn/itay/proj/GameObject.java
@@ -20,18 +20,8 @@ public class GameObject implements Disposable {
     public void updatePosition() {
         float speed = 2.5f;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            y += speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            y -= speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            x -= speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            x += speed;
-        }
+        x += GameInput.KeyForce.x * speed;
+        y += GameInput.KeyForce.y * speed;
     }
 
     public void draw(SpriteBatch batch) {
@@ -41,5 +31,13 @@ public class GameObject implements Disposable {
     @Override
     public void dispose() {
         texture.dispose();
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
     }
 }
--- LearningProj/core/src/learn/itay/proj/LearningProjGame.java
+++ LearningProj/core/src/learn/itay/proj/LearningProjGame.java
@@ -4,18 +4,16 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LearningProjGame extends ApplicationAdapter {
     SpriteBatch batch;
-//    Texture img;
-
     GameObject gameObject;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-//        img = new Texture("badlogic.jpg");
         gameObject = new GameObject("badlogic.jpg", 0, 0);
     }
 
@@ -24,16 +22,17 @@ public class LearningProjGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+
+        GameInput.update();
         gameObject.updatePosition();
         gameObject.draw(batch);
-//		batch.draw(img, 0, 0);
+
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-//        img.dispose();
         gameObject.dispose();
     }
 }

fb8f125498bcfc8cebe28817540270e7338e738e
Itay <itay647@gmail.com>
2017-12-28 14:32:08 +0200

    Changed the structure of the game to use the Game and Screen classes

---
+++ FinalDays/core/src/com/itay347/finaldays/PlayScreen.java
@@ -0,0 +1,20 @@
+package com.itay347.finaldays;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class PlayScreen extends ScreenAdapter {
+
+    public PlayScreen() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

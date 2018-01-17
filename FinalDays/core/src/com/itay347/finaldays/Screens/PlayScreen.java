c4b1b5f1b33153a48e9c13bac5e503545f732cd8
Itay <itay647@gmail.com>
2018-01-17 21:03:47 +0200

    Added the tiled map
    + Fixed the resizing of the game window (now we "update" the camera after a resize)

--- FinalDays/core/src/com/itay347/finaldays/Screens/PlayScreen.java
+++ FinalDays/core/src/com/itay347/finaldays/Screens/PlayScreen.java
@@ -2,25 +2,46 @@ package com.itay347.finaldays.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.itay347.finaldays.Actors.MyActor;
 import com.itay347.finaldays.FinalDays;
 
 public class PlayScreen extends ScreenAdapter {
+    public static final String MAP_FILE_NAME = "Maps\\FinalDaysMap.tmx";
+    public static final String TEMP_IMAGE = "badlogic.jpg";
     private FinalDays game;
 
     private Stage stage;
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
     private MyActor tempActor;
 
     public PlayScreen(FinalDays game) {
         this.game = game;
-        game.getAssetManager().load("badlogic.jpg", Texture.class);
-        game.getAssetManager().finishLoadingAsset("badlogic.jpg");
+        game.getAssetManager().load(TEMP_IMAGE, Texture.class);
+        game.getAssetManager().finishLoadingAsset(TEMP_IMAGE);
+
+//        // only needed once
+//        game.getAssetManager().setLoader(TiledMap.class, new TmxMapLoader());
+//        game.getAssetManager().load(MAP_FILE_NAME, TiledMap.class);
+//        game.getAssetManager().finishLoadingAsset(MAP_FILE_NAME);
+//        // once the asset manager is done loading
+//        tiledMap = game.getAssetManager().get(MAP_FILE_NAME);
+//        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        tiledMap = new TmxMapLoader().load(MAP_FILE_NAME);
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
         stage = new Stage();
-        tempActor = new MyActor((Texture) game.getAssetManager().get("badlogic.jpg"));
+        tempActor = new MyActor((Texture) game.getAssetManager().get(TEMP_IMAGE));
         stage.addActor(tempActor);
         Gdx.input.setInputProcessor(stage);
     }
@@ -34,12 +55,21 @@ public class PlayScreen extends ScreenAdapter {
 
         // update the stage
         stage.act(Math.min(delta, 1 / 30f));
+        // draw the map
+        tiledMapRenderer.setView((OrthographicCamera) stage.getCamera());
+        tiledMapRenderer.render();
         // draw the stage
         stage.draw();
     }
 
     @Override
+    public void resize(int width, int height) {
+        ((OrthographicCamera) stage.getCamera()).setToOrtho(false, width, height);
+    }
+
+    @Override
     public void dispose() {
         stage.dispose();
+        tiledMap.dispose();
     }
 }
--- FinalDays/desktop/src/com/itay347/finaldays/desktop/DesktopLauncher.java
+++ FinalDays/desktop/src/com/itay347/finaldays/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.itay347.finaldays.FinalDays;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 960;
+		config.height = 540;
 		new LwjglApplication(new FinalDays(), config);
 	}
 }

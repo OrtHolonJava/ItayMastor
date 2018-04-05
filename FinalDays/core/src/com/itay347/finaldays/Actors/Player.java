c7116828442f897c638a16ef0c93ab7cdda85bfe
Itay <itay647@gmail.com>
2018-04-05 17:50:30 +0300

    Lights added. The game now looks way better!
    + Fixed rotation of player not working properly when resizing the game window
    * Toggle lights with NUMPAD_1

--- FinalDays/core/src/com/itay347/finaldays/Actors/Player.java
+++ FinalDays/core/src/com/itay347/finaldays/Actors/Player.java
@@ -1,15 +1,27 @@
 package com.itay347.finaldays.Actors;
 
+import box2dLight.ConeLight;
+import box2dLight.PointLight;
+import box2dLight.RayHandler;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.itay347.finaldays.GameInput;
 
 public class Player extends BasicActor {
+    private ConeLight coneLight1;
+    private ConeLight coneLight2;
+    private PointLight pointLight;
 
-    public Player(Texture texture, World world) {
+    public Player(Texture texture, World world, RayHandler rayHandler) {
         super(texture, texture.getWidth() / 6, texture.getHeight() / 6, world);
+
+        // Init the player's lights
+        coneLight1 = new ConeLight(rayHandler, 500, new Color(1, 1, 1, 1), 1000, 0, 0, 0, 35);
+        coneLight2 = new ConeLight(rayHandler, 500, new Color(1, 1, 1, 0.2f), 1000, 0, 0, 0, 80);
+        pointLight = new PointLight(rayHandler, 100, new Color(1, 1, 1, 1), 125, 0, 0);
     }
 
     @Override
@@ -32,8 +44,14 @@ public class Player extends BasicActor {
         super.act(delta);
 
         // Rotate the player according to the mouse
-        Vector2 dir = new Vector2(Gdx.input.getX() - getStage().getWidth() / 2,
-                Gdx.input.getY() - getStage().getHeight() / 2);
+        Vector2 dir = new Vector2(Gdx.input.getX() - getStage().getCamera().viewportWidth / 2,
+                Gdx.input.getY() - getStage().getCamera().viewportHeight / 2);
         this.setRotation(-dir.angle()); // minus the angle because it's flipped without it...
+
+        coneLight1.setPosition(body.getPosition());
+        coneLight1.setDirection(-dir.angle());
+        coneLight2.setPosition(body.getPosition());
+        coneLight2.setDirection(-dir.angle());
+        pointLight.setPosition(body.getPosition());
     }
 }

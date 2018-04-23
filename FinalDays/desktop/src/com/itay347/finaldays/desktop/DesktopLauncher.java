44ec8094cfcbb212b37ec0d5419e27cc779975d6
Itay <itay647@gmail.com>
2018-04-23 16:09:21 +0300

    Added javadoc to the DesktopLauncher

--- FinalDays/desktop/src/com/itay347/finaldays/desktop/DesktopLauncher.java
+++ FinalDays/desktop/src/com/itay347/finaldays/desktop/DesktopLauncher.java
@@ -4,11 +4,19 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.itay347.finaldays.FinalDays;
 
+/**
+ * The launcher of the game for desktop
+ */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 960;
-		config.height = 540;
-		new LwjglApplication(new FinalDays(), config);
-	}
+    /**
+     * The main method which starts the game
+     *
+     * @param arg Arguments
+     */
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 960;
+        config.height = 540;
+        new LwjglApplication(new FinalDays(), config);
+    }
 }
